fbeefd1c53a56d1e36ccb238bda7be70b43a5084
Radin Dafina <rdafina12@gmail.com>
2024-12-20 14:41:26 +0800

    Update app.py

--- master/app.py
+++ master/app.py
@@ -46,10 +46,14 @@ def get_base64_images(directory):
                 base64_images.append(base64_image)
     return base64_images
 
-# Function to extract text from PDF
-def extract_text_from_pdf(pdf_file):
+def extract_text_from_pdf(uploaded_file):
     text = ""
-    pdf_document = fitz.open(stream=pdf_file.read(), filetype="pdf")
+    # Read the uploaded PDF file into bytes
+    pdf_bytes = uploaded_file.read()
+
+    # Open the PDF using PyMuPDF from the byte stream
+    pdf_document = fitz.open(stream=pdf_bytes, filetype="pdf")
+
     for page_num in range(pdf_document.page_count):
         page = pdf_document.load_page(page_num)
         text += page.get_text("text")  # Extract raw text
